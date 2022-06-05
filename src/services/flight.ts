bf28e20beabf641e010a3c5f2c7da3b355d75381
JunYeop92 <41728258+JunYeop92@users.noreply.github.com>
2022-06-05 14:53:12 +0900

    fix: 배포

--- src/services/flight.ts
+++ src/services/flight.ts
@@ -2,10 +2,9 @@ import { axios } from 'hooks/worker'
 import { IFlightItem, IFlightApiParams } from 'types/flight'
 
 const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy'
-const END_POINT = '/B551177/StatusOfPassengerFlightsOdp'
 
 export const getFlightArriveApi = async (params: IFlightApiParams): Promise<IFlightItem[]> => {
-  const res = await axios.get(`${PROXY}${END_POINT}/getPassengerArrivalsOdp`, {
+  const res = await axios.get(`${PROXY}/getPassengerArrivalsOdp`, {
     params: {
       serviceKey: process.env.REACT_APP_FLIGHT_API_KEY,
       ...params,
@@ -18,7 +17,7 @@ export const getFlightArriveApi = async (params: IFlightApiParams): Promise<IFli
 }
 
 export const getFlightDepartApi = async (params: IFlightApiParams): Promise<IFlightItem[]> => {
-  const res = await axios.get(`${END_POINT}/getPassengerDeparturesOdp`, {
+  const res = await axios.get(`${PROXY}/getPassengerDeparturesOdp`, {
     params: {
       serviceKey: process.env.REACT_APP_FLIGHT_API_KEY,
       ...params,
